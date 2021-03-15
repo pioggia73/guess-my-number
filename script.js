fc188c799ca148eb6460d63bd2a44a305d8b3580
Monika Wasilewska <monikawasilewska13@wp.pl>
2021-03-15 12:15:30 +0100

    implementing logic part 1

--- script.js
+++ script.js
@@ -1,1 +1,29 @@
 'use strict';
+
+
+const btnCheck = document.querySelector('.check');
+const message = document.querySelector('.message');
+
+
+const secretNumber = (Math.trunc(Math.random() * 20) ) + 1;
+document.querySelector('.number').textContent = secretNumber;
+
+
+btnCheck.addEventListener('click', function() {
+    const guess = Number(document.querySelector('.guess').value)
+    
+    if (!guess) {
+        message.textContent = 'No number! 😢'
+    }
+    else if (guess === secretNumber) {
+        message.textContent = 'Correct number! 😁'
+    }
+    else if (guess > secretNumber) {
+        message.textContent = 'Too high! 😒'
+    }
+    else if (guess < secretNumber) {
+        message.textContent = 'Too low! 😒'
+    }
+   
+});
+
